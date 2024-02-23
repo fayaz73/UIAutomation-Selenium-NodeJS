bd9645954272e46f000498ab62cca00d49078f8b
fayaz73 <49472430+fayaz73@users.noreply.github.com>
2024-02-24 01:28:24 +0530

    Update logintest.spec.js

--- test/logintest.spec.js
+++ test/logintest.spec.js
@@ -3,7 +3,6 @@ const { Builder, By, WebElementCondition,Capabilities, until, Actions } = requir
 const testdata = require('./../objects/testData')
 const assert = require("assert")
 const loginpage = require('../pageobjects/loginpage');
-const { drive } = require("googleapis/build/src/apis/drive");
 const objt = require('../operations/ReadObjects');
 const opertn = require('../operations/UIOperations');
 
