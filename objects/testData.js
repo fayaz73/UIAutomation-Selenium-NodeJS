8acdaa9bfec1ba2001375d2cc1fb1a2c5c966899
fayaz73 <49472430+fayaz73@users.noreply.github.com>
2024-02-24 01:07:45 +0530

    Update testData.js

--- objects/testData.js
+++ objects/testData.js
@@ -1,6 +1,6 @@
 var expectedResult = require('./expectedOP');
-const username = { input: { validusername: 'fayaz_4812_qapostprod', invalidusername: { specialCharacter: '', onlyNumber: '', onlyString: '' } }, expectedOutput : expectedResult.ExpedtedloginMsg};
-const password = { input: { validpassword: 'Falih@73', invalidusername: { specialCharacter: '1233#@$U', onlyNumber: '', onlyString: '' } } , expectedOutput : expectedResult.ExpedtedloginMsg};
+const username = { input: { validusername: 'ahamed6035', invalidusername: { specialCharacter: '', onlyNumber: '', onlyString: '' } }, expectedOutput : expectedResult.ExpedtedloginMsg};
+const password = { input: { validpassword: '123$Urbanladder', invalidusername: { specialCharacter: '1233#@$U', onlyNumber: '', onlyString: '' } } , expectedOutput : expectedResult.ExpedtedloginMsg};
 const logOutMsg = { input: {},expectedOutput: expectedResult.ExpectedlogoutMsg };
 const segmentListPage = { input: {},expectedOutput: expectedResult.ExpectedSegmentListPage };
 const segmentCreatePage = {input: {}, expectedOutput: expectedResult.ExpectedSegmentCreated };
