9a09acacc36451b39fe486685a091146eab7debf
fayaz73 <49472430+fayaz73@users.noreply.github.com>
2024-02-24 07:20:03 +0530

    Update segmentlisttest.spec.js

--- test/segmentlisttest.spec.js
+++ test/segmentlisttest.spec.js
@@ -3,8 +3,7 @@ const { Builder, By, WebElementCondition,Capabilities, until, Actions } = requir
 const testdata = require('./../objects/testData')
 const assert = require("assert")
 const loginpage = require('../pageobjects/loginpage');
-const segmentlistpage = require('../pageobjects/segmentlistpage')
-const { drive } = require("googleapis/build/src/apis/drive");
+const segmentlistpage = require('../pageobjects/segmentlistpage');
 const objt = require('../operations/ReadObjects');
 const opertn = require('../operations/UIOperations');
 
