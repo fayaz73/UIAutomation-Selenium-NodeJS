c59359506060773f542f58e0205834918af0f692
fayaz73 <49472430+fayaz73@users.noreply.github.com>
2024-02-25 23:37:17 +0530

    Update logintest.js

--- test/logintest.js
+++ test/logintest.js
@@ -1,4 +1,10 @@
+
+const { Builder, By, WebElementCondition,Capabilities, until, Actions } = require("selenium-webdriver")
+const testdata = require('./../objects/testData')
+const assert = require("assert")
 const loginpage = require('../pageobjects/loginpage');
+const objt = require('../operations/ReadObjects');
+const opertn = require('../operations/UIOperations');
 
 
 function login() {
