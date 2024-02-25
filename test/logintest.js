2454f57d5737939daf963ac98c9608e9c1fef227
fayaz73 <49472430+fayaz73@users.noreply.github.com>
2024-02-26 01:13:16 +0530

    Update logintest.js

--- test/logintest.js
+++ test/logintest.js
@@ -6,10 +6,13 @@ function  login() {
     driver.get("https://ttl.lemnisk.co/app")
     console.log("executed first step")
     driver.manage().window().setRect({ width: 1848, height: 1053 })
-    console.log("executed second step"5
+    console.log("executed second step")
     driver.findElement(By.name("username")).sendkeys("ahamed6035");
+    console.log("executed third step")
     driver.findElement(By.name("password")).sendKeys("123$Urbanladder"));
+    console.log("executed fourth step")
     driver.findElement(By.xpath("//button")).click();
+    console.log("executed fifth step")
 }
 
 login();
