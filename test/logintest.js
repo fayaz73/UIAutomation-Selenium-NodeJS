9a60ba09799871ca946992d2f2b9e7475498b299
fayaz73 <49472430+fayaz73@users.noreply.github.com>
2024-02-26 00:36:46 +0530

    Update logintest.js

--- test/logintest.js
+++ test/logintest.js
@@ -1,15 +1,10 @@
-
-const { Builder, By, WebElementCondition,Capabilities, until, Actions } = require("selenium-webdriver")
-const testdata = require('./../objects/testData')
-const assert = require("assert")
-const loginpage = require('../pageobjects/loginpage');
-const objt = require('../operations/ReadObjects');
-const opertn = require('../operations/UIOperations');
+var webdriver = require('selenium-webdriver');
+var  driver =  new webdriver.Builder().forBrowser('chrome').build();
 
 
-function async login() {
-    await driver.get("https://www.google.com")
-    await driver.manage().window().setRect({ width: 1848, height: 1053 })
+function  login() {
+    driver.get("https://www.google.com")
+    driver.manage().window().setRect({ width: 1848, height: 1053 })
 
 }
 
