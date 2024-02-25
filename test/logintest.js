b71389235790f40e66131bfec0e9813d4fac6096
fayaz73 <49472430+fayaz73@users.noreply.github.com>
2024-02-26 00:59:32 +0530

    Update logintest.js

--- test/logintest.js
+++ test/logintest.js
@@ -4,7 +4,9 @@ var  driver =  new webdriver.Builder().forBrowser('chrome').build();
 
 function  login() {
     driver.get("https://www.google.com")
+    console.log("executed first step")
     driver.manage().window().setRect({ width: 1848, height: 1053 })
+    console.log("executed second step")
 
 }
 
