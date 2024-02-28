b7879e81960a3514f96962d0cefd81f558e8a506
fayaz73 <49472430+fayaz73@users.noreply.github.com>
2024-02-28 16:30:08 +0530

    Update logintest.js

--- test/logintest.js
+++ test/logintest.js
@@ -2,8 +2,8 @@ var webdriver = require('selenium-webdriver');
 const { Builder, By, WebElementCondition,Capabilities, until, Actions } = require("selenium-webdriver")
 var  driver =  new webdriver.Builder().forBrowser('chrome').build();
 
-/*
-describe('Testing login functionality', function() {
+
+describe('Testing login functionality', async function() {
   this.timeout(100000)
     it('login_test', async function() {
       
@@ -20,19 +20,3 @@ describe('Testing login functionality', function() {
   })
      })
 
-     */
-function login() {
-  
-    await driver.get("https://ttl.lemnisk.co/app")
-    console.log("executed first step")
-    await driver.manage().window().setRect({ width: 1848, height: 1053 })
-    console.log("executed second step")
-    await driver.findElement(By.name("username")).sendKeys("ahamed6035");
-    console.log("executed third step")
-    await driver.findElement(By.name("password")).sendKeys("123$Urbanladder");
-    console.log("executed fourth step")
-    await driver.findElement(By.xpath("//button")).click();
-    console.log("executed fifth step")
-}
-
-login()
